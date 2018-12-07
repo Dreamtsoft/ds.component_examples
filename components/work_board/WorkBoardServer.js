c844fb08d2db1fb27adee02535249a0a345111d3
donovanstone <donovan@dreamtsoft.com>
2018-12-06 19:17:05 -0800

    Adding choices on to workboard and including a "no value" if the slot def None = true

--- components/work_board/WorkBoardServer.js
+++ components/work_board/WorkBoardServer.js
@@ -1,74 +1,95 @@
-var PageComponent = require("ds.base/PageComponent");
+var PageComponent = require('ds.base/PageComponent');
 
 var KanbanBoardServer = PageComponent.create({
 	data: function(attributes, vars, containerList) {
 		var realJSON = [];
 		this.boards = [];
 		this.condition = attributes.condition;
-        this.groupBy = attributes.group_by;
-        this.bucketName = attributes.bucket;
+		this.groupBy = attributes.group_by;
+		this.bucketName = attributes.bucket;
 
-        this._createBoards();
+		this._createBoards();
 
 		return new StatusResponse('good', {
-			boards: this.boards,
+			boards: this.boards
 		});
 	},
 
 	_createBoards: function() {
 		//Find the group by values for the boards
+		console.log('GROUPBY ' + this.groupBy);
 		var boardsObject = {};
 		var bc = new FRecord(this.bucketName);
-        if (this.condition) {
-            bc.addEncodedSearch(this.condition);
-        }
-		bc.search();
-		while (bc.next()) {
-            var boardValue = bc.getValue(this.groupBy);
-            console.log('BOARDVALUE: ' + boardValue);
-            var boardName = "No Value";
-            if (!Object.isNil(boardValue)) {
-                var boardName = bc.getDisplayValue(this.groupBy);
+		var sd = bc.getSlotDefinition(this.groupBy);
+		var none_opt = sd.attributes.none_option;
+		var choices = JSON.stringify(sd.attributes.choices);
+		console.log('SLOT DEFINITION' + JSON.stringify(sd));
+		console.log("NONE OPTION "+ sd.attributes.none_option);
+		if (sd.type == 'choice') {
+			if(none_opt === true){
+			boardsObject[0] = "No Value";
 			}
-            if (boardsObject[boardValue] === undefined) {
-                boardsObject[boardValue] = boardName;
-            }
-
-
-        }
-
-        //Now iterate through the values and create boards
-        for (var board in boardsObject) {
-            console.log(board + " - " + boardsObject[board]);
-            this.boards.push(this._createBoard(board, boardsObject[board]));
-
-        }
+			console.log(boardsObject[0]);
+			for (var i = 0; i < sd.attributes.choices.length; i++) {
+				var len = i +1;
+				var choice = sd.attributes.choices[i];
+				console.log("CHOICE LABEL" + choice.label + " CHOICE VALUE " + choice.value);
+				boardsObject[len] = choice.label;
+			}
+		} else {
+			if (this.condition) {
+				bc.addEncodedSearch(this.condition);
+			}
+			bc.search();
+			while (bc.next()) {
+				var boardValue = bc.getValue(this.groupBy);
+				console.log('BOARDVALUE: ' + boardValue);
+				var boardName = 'No Value';
+				if (!Object.isNil(boardValue)) {
+					var boardName = bc.getDisplayValue(this.groupBy);
+				}
+				if (boardsObject[boardValue] === undefined) {
+					boardsObject[boardValue] = boardName;
+				}
+			}
+		}
+		//Now iterate through the values and create boards
+		console.log("JSON STIRINGIFY BOBJECT" + JSON.stringify(boardsObject));
+		for (var board in boardsObject) {
+			console.log(board + ' - ' + boardsObject[board]);
+			this.boards.push(this._createBoard(board, boardsObject[board]));
+		}
 	},
 
 	_createBoard: function(id, title) {
+		console.log("STARTING CREATE BOARD");
+		console.log("CREATE BOARD ID = " + id + " LABEL/TITLE " + title);
 		var board = {};
 		board.id = id;
 		board.title = title;
-		board.class = "info";
-        //board.dragTo = ['_working'];
+		board.class = 'info';
+		//board.dragTo = ['_working'];
 
 		//Now go get the items
-        var itemsArray = [];
-        var ic = new FRecord(this.bucketName);
-        if (this.condition) {
-            ic.addEncodedSearch(this.condition);
-        }
-        ic.addSearch(this.groupBy, id);
-        ic.search();
-        while (ic.next()) {
-            itemsArray.push(this._createItem(ic.id, ic.getRecordDisplayValue() + ": " + ic.title));
-        }
+		var itemsArray = [];
+		var ic = new FRecord(this.bucketName);
+		if (this.condition) {
+			ic.addEncodedSearch(this.condition);
+		}
+		ic.addSearch(this.groupBy, id);
+		ic.search();
+		while (ic.next()) {
+			itemsArray.push(
+				this._createItem(
+					ic.id,
+					ic.getRecordDisplayValue() + ': ' + ic.title
+				)
+			);
+		}
 
-        board.item = itemsArray;
+		board.item = itemsArray;
 
-        return board;
-
-
+		return board;
 
 		/*
         {
@@ -130,10 +151,9 @@ var KanbanBoardServer = PageComponent.create({
                 }
             },
 		 */
-
 	},
-	
-	type: "KanbanBoardServer"
+
+	type: 'KanbanBoardServer'
 });
 
 module.exports = KanbanBoardServer;
